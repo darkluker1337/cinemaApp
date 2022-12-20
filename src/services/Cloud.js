4e6d52fed0791ae767b0381051dc1ee0b23413e9
Pavel Emelyanov <lictratenkodenis26@gmail.com>
2022-12-20 19:38:38 +0300

    end

--- src/services/Cloud.js
+++ src/services/Cloud.js
@@ -4,10 +4,10 @@ export class CloudService {
   constructor() {
     this.config = {
       apiKey: process.env.API_KEY,
-      authDomain: "online-cinema-fb342.firebaseapp.com",
-      projectId: "online-cinema-fb342",
-      storageBucket: "online-cinema-fb342.appspot.com",
-      messagingSenderId: "1006611980075",
+      authDomain: "cinema-app-cb1f7.firebaseapp.com",
+      projectId: "cinema-app-cb1f7",
+      storageBucket: "cinema-app-cb1f7.appspot.com",
+      messagingSenderId: "336276762016",
       appId: process.env.APP_ID
     };
 
